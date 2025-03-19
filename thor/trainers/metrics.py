1a2005bc0a407d7de7cab301f98a6e19fb6f0d9b
THOR <theo.moreau@octo.com>
2025-03-19 10:03:33 +0100

    Update metric (set compute instead of batch)

--- thor/trainers/metrics.py
+++ thor/trainers/metrics.py
@@ -1,8 +1,7 @@
 import typing
 from abc import ABC, abstractmethod
 
-from torcheval.metrics.functional import multiclass_f1_score, multiclass_precision, multiclass_recall
-from torcheval.metrics import MulticlassAUPRC
+from torcheval.metrics import MulticlassRecall, MulticlassPrecision, MulticlassF1Score, MulticlassAUPRC
 import torch
 
 class Metric(ABC):
@@ -20,23 +19,22 @@ class ClassificationMetrics(Metric):
     def __init__(self, nb_classes: int, selected_metrics: list[str]):
         super().__init__(selected_metrics)
         self.nb_classes = nb_classes
-        self.function_mapping = {"f1": multiclass_f1_score, 
-                                 "precision": multiclass_precision,
-                                 "recall": multiclass_recall,
-                                 "AP": self.AP
-                                }
+        self.cls_mapping = {"f1": MulticlassF1Score(average="macro", num_classes=self.nb_classes),
+                                 "precision": MulticlassPrecision(average="macro", num_classes=self.nb_classes),
+                                 "recall": MulticlassRecall(average="macro", num_classes=self.nb_classes)
+                                 "AP": MulticlassAUPRC(num_classes=self.nb_classes)
+                                } 
     
-    def compute_metrics(self, output_logits, labels, samples_set="train"):
+    def compute_metrics(self, dataloader, output_logits, labels, samples_set="train"):
         computed_metrics = {}
         _, predicted = torch.max(output_logits, 1)
         for metric in self.selected_metrics:
-            metric_fn = self.function_mapping[metric]
-            result = metric_fn(output_logits, labels, num_classes=self.nb_classes)
+            for batch in dataloader:
+                images, _, labels = batch
+                metric = self.function_mapping[metric]
+                metric.update(output_logits, labels)
+            result = metric.compute()
+            metric.reset()
             computed_metrics[samples_set + "_" + metric] = result
         
         return computed_metrics
-    
-    def AP(self, output_logits, labels, num_classes=None):
-        average_precision = MulticlassAUPRC(num_classes=num_classes)
-        average_precision.update(output_logits, labels)
-        return average_precision.compute()
