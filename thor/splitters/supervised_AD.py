9bf6e19d2f550973978379bdf18b03d2fecb54e2
THOR <theo.moreau@octo.com>
2025-03-19 15:11:36 +0100

    Feat(splitters): Add supervised splitter for image classification

--- thor/splitters/supervised_AD.py
+++ thor/splitters/supervised_AD.py
@@ -4,7 +4,7 @@ import glob
 from .supervised_base import MVTech_SP_split
 
 
-class MVTech_AD_supervised_split(MVTech_SP_split):
+class MVTech_AD_supervised_seg_split(MVTech_SP_split):
     def __init__(self, dataset_path, classname, train_split, val_split, dist_adjust=False, multiclass=False):
         super().__init__(dataset_path, classname, train_split, val_split, dist_adjust, multiclass)
         
@@ -33,8 +33,34 @@ class MVTech_AD_supervised_split(MVTech_SP_split):
             self.class_mapping["1"] = "Defect"
         self.nb_defect_samples += len(self.defect_samples[str(i+1)])
 
+
+class MVTech_AD_supervised_cls_split(MVTech_SP_split):
+    def __init__(self, dataset_path, classname, train_split, val_split, dist_adjust=False, multiclass=False):
+        super().__init__(dataset_path, classname, train_split, val_split, dist_adjust, multiclass)
         
-class MVTech_LOCO_supervised_split(MVTech_SP_split):
+    def create_samples(self):
+      self.class_mapping["0"] = "No defect"
+
+      for i, defect_class in enumerate(self.defect_classes):
+        image_path = os.path.join(self.root_dir, "test/", defect_class)
+
+        images = glob.glob(os.path.join(image_path, '*.png'))
+        images.sort()
+
+        self.defect_samples[str(i+1)] = []
+        if self.multiclass:
+            for image in images:
+              self.defect_samples[str(i+1)].append((image, i+1))
+
+            self.class_mapping[str(i+1)] = defect_class
+        else:
+          for image in images:
+            self.defect_samples[str(i+1)].append((image, 1))
+            self.class_mapping["1"] = "Defect"
+        self.nb_defect_samples += len(self.defect_samples[str(i+1)])
+
+
+class MVTech_LOCO_supervised_seg_split(MVTech_SP_split):
     def __init__(self, dataset_path, classname, train_split, val_split, dist_adjust=False, multiclass=False):
         super().__init__(dataset_path, classname, train_split, val_split, dist_adjust, multiclass)
         
